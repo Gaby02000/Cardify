5ea4fec3b0db2d0a52cf2dddc80091057687b82c
unknown <Alejo Gonzalez>
2025-06-08 20:22:29 -0300

    Modificado el cors.php para permitir tambien acceder desde el deploy de vercel

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['apis/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => ['http://localhost:5173', 'https://cardify-frontend-git-dev-gaby02000s-projects.vercel.app'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
