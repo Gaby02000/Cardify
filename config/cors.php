92981c129ffea5e007d514f146534f9f352e82ba
unknown <Alejo Gonzalez>
2025-06-08 20:39:37 -0300

    Cambios a allowed_origins en cors.php

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,8 @@
 return [
     'paths' => ['apis/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173', 'https://cardify-frontend-git-dev-gaby02000s-projects.vercel.app'],
+    // 'allowed_origins' => ['http://localhost:5173', 'https://cardify-frontend-git-dev-gaby02000s-projects.vercel.app'],
+    'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
