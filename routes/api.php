7a737505c7d6614fefe2ae4bbcca9be3f9732b2c
Gabriel Federico Jose Gimenez Miguel <gabylan02000@hotmail.com>
2025-06-21 00:54:04 -0300

    feature: register de usuario

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\Api\CartApiController;
 use App\Http\Controllers\Api\CartItemApiController;
 use App\Http\Controllers\Api\OrderApiController;
 use App\Http\Controllers\Api\LoginApiController;
+use App\Http\Controllers\Api\UserClientAuthController;
+use App\Models\User;
 use Illuminate\Session\Middleware\StartSession;
 
 Route::get('/giftcards', [GiftCardApiController::class, 'index']);
@@ -15,12 +17,13 @@ Route::get('/categories', [CategoryApiController::class, 'index']);
 Route::middleware(['api', StartSession::class])->group(function () {
     Route::post('/login', [LoginApiController::class, 'login']);
     Route::post('/logout', [LoginApiController::class, 'logout']);
-
+    Route::post('/register', [UserClientAuthController::class, 'register']);
     Route::get('/cart', [CartApiController::class, 'show']);
     Route::post('/cart/add-item', [CartApiController::class, 'addItem']);
     Route::post('/cart/clear', [CartApiController::class, 'clear']);
     Route::put('/cart-item/{cartItem}', [CartItemApiController::class, 'update']);
     Route::delete('/cart-item/{cartItem}', [CartItemApiController::class, 'destroy']);
+    
     Route::post('/orders', [OrderApiController::class, 'store']);
     Route::get('/orders/{order}', [OrderApiController::class, 'show']);
     });
